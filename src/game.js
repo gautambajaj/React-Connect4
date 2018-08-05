8be5ee8216f8ac17a1f3b6323090988d6e20442d
Gautam Bajaj <gautam.bajaj@uwaterloo.ca>
2018-08-05 11:44:44 +0530

    grid row components added

---
+++ src/game.js
@@ -0,0 +1,10 @@
+var App = React.createClass({
+  render: function() {
+    return <div>Something something Dark Side</div>;
+  }
+});
+ 
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
